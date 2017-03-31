f639e6102b17a4081690d114950bf2dd333644a3
walensep <walen_almeida@hotmail.com>
2017-03-31 13:28:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,15 @@ function main() {
 	
 	$('#txtTraduzido').attr('readonly', true);
 	
+	$('#txtTraduzir').keyup(function(){
+    		this.value = this.value.toUpperCase();
+	});
+	
 	$('#btnTraduzir').on('click', function() {
 		traduzir = $('#txtTraduzir').val();
-		traduzido = traduzir.replace(/lh/g, "li").replace(/ç/g,"s").replace(/ss/g, "ç").replace(/ão/g,"aum").replace(/ãe/g,"aie").replace("ó","oh");
+		traduzido = traduzir.replace(/ACABOU/g,"ACOBOU").replace(/PALMEIRAS/g, "SEM MUNDIAL").replace(/CORINTHIANS/g,"GALINHADA").replace(/SANTOS/g,"VIÚVAS DO PELÉ").replace(/SÃO PAULO/g,"BAMBIS");
+		traduzido = traduzido.replace(/NAMORADO/g, "DOLLYNALDO").replace(/NAMORADA/g, "DOLLYNETE");
+		traduzido = traduzido.replace(/LH/g, "LI").replace(/Ç/g,"S").replace(/SS/g, "Ç").replace(/ÃO/g,"AUM").replace(/ÃE/g,"AIE").replace(/Ó/g,"OH");
 		$('#txtTraduzido').val(traduzido);	
 	});
 }
