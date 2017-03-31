1cd45458ae8175f35c2189c38355be2c92726c84
walensep <walen_almeida@hotmail.com>
2017-03-31 11:55:55 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+function main() {
+	var traduzir = '';
+	var traduzido = '';
+	
+	$('#txtTraduzido').attr('readonly', true);
+	
+	$('#btnTraduzir').on('click', function() {
+		traduzir = $('#txtTraduzir').val();
+		traduzido = traduzir.replace(/lh/g, "li").replace(/ç/g,"s").replace(/ss/g, "ç").replace(/ão/g,"aum").replace(/ãe/g,"aie").replace("ó","oh");
+		$('#txtTraduzido').val(traduzido);	
+	});
+}
 
+$(document).ready(main);
