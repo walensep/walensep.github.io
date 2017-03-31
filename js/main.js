fb253dadaaf9e47d5f823983786cb7339bae8c7c
walensep <walen_almeida@hotmail.com>
2017-03-31 13:57:38 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ function main() {
 		traduzir = $('#txtTraduzir').val().toUpperCase();
 		$('#txtTraduzir').val(traduzir);
 		traduzido = traduzir.replace(/ACABOU/g,"ACOBOU").replace(/PALMEIRAS/g, "SEM MUNDIAL").replace(/CORINTHIANS/g,"GALINHADA").replace(/SANTOS/g,"VIÚVAS DO PELÉ").replace(/SÃO PAULO/g,"BAMBIS");
-		traduzido = traduzido.replace(/NAMORADO/g, "DOLLYNALDO").replace(/NAMORADA/g, "DOLLYNETE");
-		traduzido = traduzido.replace(/LH/g, "LI").replace(/Ç/g,"S").replace(/SS/g, "Ç").replace(/ÃO/g,"AUM").replace(/ÃE/g,"AIE").replace(/Ó/g,"OH");
+		traduzido = traduzido.replace(/O /g, "U ").replace(/Z /g,"S ").replace(/E/g, "I ");
+		traduzido = traduzido.replace(/NAMORADO/g, "DOLLYNALDO").replace(/NAMORADA/g, "DOLLYNETE").replace(/AVÔ/g, "DOLLYVO").replace(/AVÓ/g, "DOLLYAVO").replace(/PAI/g, "DOLLYPAE").replace(/MÃE/g, "DOLLYMAIN");
+		traduzido = traduzido.replace(/LH/g, "LI").replace(/Ç/g,"S").replace(/SS/g, "Ç").replace(/ÃO/g,"AUM").replace(/ÃE/g,"AIN").replace(/Ó/g,"OH");
 		$('#txtTraduzido').val(traduzido);	
 	});
 }
