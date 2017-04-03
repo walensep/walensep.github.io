0768f2a158a775c674c1508142a455835fc11304
walensep <walen_almeida@hotmail.com>
2017-04-03 13:22:04 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ function main() {
 	$('#txtTraduzido').attr('readonly', true);
 	
 	$('#btnTraduzir').on('click', function() {
-		traduzir = $('#txtTraduzir').val().toUpperCase();
+		traduzir = $('#txtTraduzir').val().toUpperCase() + " ";
 		$('#txtTraduzir').val(traduzir);
 		traduzido = traduzir.replace(/ACABOU/g,"ACOBOU").replace(/PALMEIRAS/g, "SEM MUNDIAL").replace(/CORINTHIANS/g,"GALINHADA").replace(/SANTOS/g,"VIÚVAS DO PELÉ").replace(/SÃO PAULO/g,"BAMBIS");
-		traduzido = traduzido.replace(/O\s/g, "U ").replace(/E\s/g, "I ").replace(/Z\s/g, "S ");
-		traduzido = traduzido.replace(/NAMORADO/g, "DOLLYNALDO").replace(/NAMORADA/g, "DOLLYNETE").replace(/AVÔ/g, "DOLLYVO").replace(/AVÓ/g, "DOLLYAVO").replace(/PAI/g, "DOLLYPAE").replace(/MÃE/g, "DOLLYMAIN");
+		traduzido = traduzido.replace(/O\s/g, "U ").replace(/E\s/g, "I ").replace(/Z\s/g, "S ").replace(/É\s/g, "EH ").replace(/M\s/g, "N ");
+		traduzido = traduzido.replace(/FILHO/g, "CATARRENTO").replace(/CRIANÇA/g, "CATARRENTO").replace(/NAMORADO/g, "DOLLYNALDO").replace(/NAMORADA/g, "DOLLYNETE").replace(/AVÔ/g, "DOLLYVO").replace(/AVÓ/g, "DOLLYAVO").replace(/PAI/g, "DOLLYPAE").replace(/MÃE/g, "DOLLYMAIN");
 		traduzido = traduzido.replace(/LH/g, "LI").replace(/Ç/g,"S").replace(/SS/g, "Ç").replace(/ÃO/g,"AUM").replace(/ÃE/g,"AIN").replace(/Ó/g,"OH");
 		$('#txtTraduzido').val(traduzido);	
 	});
