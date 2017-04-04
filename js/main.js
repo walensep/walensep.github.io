cf6437dd5f4f945de5eba9d7c06933e977444340
walensep <walen_almeida@hotmail.com>
2017-04-04 15:02:50 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function main() {
 		traduzir = $('#txtTraduzir').val().toUpperCase() + " ";
 		$('#txtTraduzir').val(traduzir);
 		traduzido = traduzir.replace(/ACABOU/g,"ACOBOU").replace(/PALMEIRAS/g, "SEM MUNDIAL").replace(/CORINTHIANS/g,"GALINHADA").replace(/SANTOS/g,"VIÚVAS DO PELÉ").replace(/SÃO PAULO/g,"BAMBIS");
-		traduzido = traduzido.replace(/MÃE\sSOLTEIRA/, "DOLLYPOLTA");
+		traduzido = traduzido.replace(/MÃE\sSOLTEIRA/, "DOLLYPOLTA").replace(/DROGADO/, "ZÉ1 DROGUINHA");
 		traduzido = traduzido.replace(/GORDO/g, "DOLLYFAT").replace(/GORDA/g, "DOLLYFAT").replace(/CACHORRO/g, "PULGUENTO1").replace(/GATO/g, "DOLLYGATO").replace(/SEXO/g, "CRUZO1");
 		traduzido = traduzido.replace(/FILHO/g, "CATARRENTO").replace(/CRIANÇA/g, "CATARRENTO").replace(/NAMORADO/g, "DOLLYNALDO").replace(/NAMORADA/g, "DOLLYNETE").replace(/AVÔ/g, "DOLLYVOH").replace(/AVÓ/g, "DOLLYAVOH").replace(/PAI/g, "DOLLYPAE").replace(/MÃE/g, "DOLLYMAIN");
 		traduzido = traduzido.replace(/CH/g,"X").replace(/LH/g, "LI").replace(/LIA/g,"LHA").replace(/Ç/g,"S").replace(/SS/g, "Ç").replace(/ÃE/g,"AIN").replace(/Ó/g,"OH");
@@ -16,7 +16,7 @@ function main() {
 		traduzido = traduzido.replace(/CU\s/g,"COOL ").replace(/COCA/, "RATO");
 		traduzido = traduzido.replace(/COMER/g,"COMAR").replace(/DIZER/g,"DIGAR");
 		traduzido = traduzido.replace(/ESCREVER/g, "ESCREVINHAR");
-		traduzido = traduzido.replace(/O1/g,"O");
+		traduzido = traduzido.replace(/O1/g,"O").replace(/É1/g, "É");
 		$('#txtTraduzido').val(traduzido);	
 	});
 }
